e81ef1b4f60384d0bb3535a16e8348a71f5ab369
Ricardo Guerra <RikkuG491@gmail.com>
2023-10-28 20:31:33 -0400

    Updated code for lecture

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -3,19 +3,15 @@ import {useState} from "react"
 function Pet({pet, adoptPet}){
 
     const [favorite, setFavorite] = useState(false)
+
+    function toggleFavorite(){
+        setFavorite((favorite) => !favorite)
+    }
     
     return (
         <li className="pet">
             <img src={pet.image} alt={pet.name} />
-            {favorite ? (
-            <button onClick={() => {
-                setFavorite((favorite) => !favorite)
-            }} className="favorite-button active">★</button>
-            ) : (
-            <button onClick={() => {
-                setFavorite((favorite) => !favorite)
-            }} className="favorite-button">☆</button>
-            )}
+            <button onClick={toggleFavorite} className={favorite ? "favorite-button active" : "favorite-button"}>{favorite ? '★' : '☆'}</button>
             <h4>{/* Write a ternary expression to display either the name or the type of animal for the pet here */}</h4>
             <button className="likes-button">{/* Number of likes goes here */} likes</button>
             <br/><br/>
